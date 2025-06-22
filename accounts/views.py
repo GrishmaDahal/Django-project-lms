90fdcb4ab8a47817b1a7ea70d29784d7d6add1ea
Grishma <grishmaadaahaal100@gmail.com>
2025-06-22 13:44:13 +0545

    Django Project LMS creating requirements.txt and installing gunicorn

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-class ProfileView(TemplateView):
-    template_name= 'students/profile.html'
+# Create your views here.
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'students/profile.html'
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
--- lms/urls.py
+++ lms/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/',include('accounts.urls')),
-    path('accounts/',include('django.contrib.auth.urls'))
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('lmsapi.urls')),
     
 ]
